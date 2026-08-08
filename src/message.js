0acabe0d97652c25d23b35e6c65cd2844be714d4
Hc <rna376388@gmail.com>
2026-08-09 05:06:36 +0800

    update

--- src/message.js
+++ src/message.js
@@ -11,11 +11,24 @@ import { fileURLToPath } from 'url';
 import PhoneNumber from 'awesome-phonenumber';
 
 import { checkStatus } from './database.js';
-import { imageToWebp, videoToWebp, writeExif, gifToWebp } from '../lib/exif.js';
+import { imageToWebp, videoToWebp, writeExif, gifToWebp } from '../lib/converter.js';
 import { getBuffer, getSizeMedia, fetchJson, sleep, axiosss, fixBytes } from '../lib/function.js';
 import { jidNormalizedUser, proto, getBinaryNodeChildren, getBinaryNodeChildString, getBinaryNodeChild, generateMessageIDV2, jidEncode, encodeSignedDeviceIdentity, generateWAMessageContent, generateForwardMessageContent, prepareWAMessageMedia, delay, areJidsSameUser, extractMessageContent, generateMessageID, downloadContentFromMessage, generateWAMessageFromContent, jidDecode, generateWAMessage, toBuffer, getContentType, getDevice } from '';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+const sockPath = fileURLToPath(new URL('../Hc.js', import.meta.url));
+
+let sockHandler = null;
+const botStartTime = Date.now();
+const groupMetadataTimers = {};
+const reloadHandler = async () => {
+  try {
+    sockHandler = (await import(`../Hc.js?update=${Date.now()}`)).default;
+  } catch (err) {
+    console.error(chalk.redBright(`[ERROR] ${err}`));
+  }
+}:
+reloadHandler();
 
 // Sampai di sini dulu esok lanjut lagi 
