f7f9ba3b040e18f3e24a22f641378f548f68057c
Hc <rna376388@gmail.com>
2026-08-10 02:12:40 +0800

    update message.js

--- src/message.js
+++ src/message.js
@@ -30,5 +30,39 @@ const reloadHandler = async () => {
   }
 };
 reloadHandler();
-
+async function GroupUpdate (sock, m, store) {
+  function clearParse(parse) {
+    try {
+      return JSON.parse(parse);
+    } catch {
+      return parse;
+    }
+  }
+  if (!m.messageStubType || !m.isGroup) return 
+  if (global.db?.groups?.[m.chat] && store?.groupMetadata?.[m.chat]) {
+    const admin = `@${m.sender.split('@')[0]}`
+    const metadata = store.groupMetadata[m.chat];
+    const normalizedTarget = clearParse(m.messageStubParameters[0]);
+    const type = m.messageStupType;
+    const messages = {
+      1: 'mereset link grup!',
+      2: `megubah Subject Grup menjadi :\n*${normalizedTarget}*`,
+      3: 'telah megubah icon grup.',
+      4: 'mereset link grup!',
+      5: `mengubah deskripsi grup.\n\n${normalizedTarget}`,
+      6: `telah mengatur agar *${normalizedTarget == 'on' ? 'hanya admin' : 'semua peserta'}* yang dapat mengedit info grup.`,
+      7: `telah *${normalizedTarget == 'on' ? 'menutup' : 'membuka'}* grup!\nSekarang ${normalizedTarget == 'on' ? 'hanya admin yang' : 'semua peserta'} dapat mengirim pesan.`,
+      8: `telah menjadikan @${normalizedTarget?.id?.split('@')?.[0]} sebagai admin.`,
+      9: `telah menghentikan @${normalizedTarget?.id?.split('@')?.[0]} dari admin.`,
+      10: `mengubah durasi pesan sementara menjadi *@${normalizedTarget}*`,
+      11: 'menonaktifkan pesan sementara.',
+      12: 'mereset link grup!',
+      13: `@${normalizedTarget.on?.split('@')?.[0]} meminta bergabung`,
+    }
+    if (sock.public && global.db?.groups?.[m.chat]?.setinfo && messages[type]) {
+      await sock.
+    }
+  }
+}
+// Follow akun ig ponyndo1_original dulu gak sih
 // Sampai di sini dulu esok lanjut lagi 
