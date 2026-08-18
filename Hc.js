162a02a2a2581d55e6e5c7727080a6ac627d0bdc
Hc <rna376388@gmail.com>
2026-08-19 01:58:38 +0800

    update

--- Hc.js
+++ Hc.js
@@ -1,6 +1,12 @@
-switch (command) {
+async function Hc(hc, m, db) {
+  switch (command) {
   case 'tes': {
   await sock.sendMessage(sender, { text: 'Ya\nsayang' }, { quoted: m });
   }
   break
 }
+
+
+}
+
+export { Hc };
