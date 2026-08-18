b9ad7b37cd0b1509ceaf26527634d7ab59a562a6
Hc <rna376388@gmail.com>
2026-08-18 22:55:45 +0800

    update

---
+++ index.js
@@ -0,0 +1,33 @@
+/*
+   * index.js
+   * By Heart candy
+   * Sc ini open source
+*/
+import fs from 'fs';
+import os from 'os';
+import pino from 'pino';
+import readiline from 'readline';
+import makeWaSocket, { useMultiFileAuthState, fetchLatestBaileysVersion, makeCachebleSignalkeyStore, Browser } from '@whiskeysockets/baileys';
+
+import { loadDatabase, saveDatabase } from './database.js';
+
+const rl = readiline.createInterface({ input: process.stdin, output: process.stdout });
+const question = (text) => new Promise((resolve) => rl.questinon(text, resoleve));
+
+
+
+function displaySystemInfo() {
+    console.log(chalk.red.bold(`
+    ██╗  ██╗ ██████╗          ██████╗  ██████╗ ████████╗
+    ██║  ██║██╔════╝          ██╔══██╗██╔═══██╗╚══██╔══╝
+    ███████║██║     ████████╗ ██████╔╝██║   ██║   ██║   
+    ██╔══██║██║     ╚═══════╝ ██╔══██╗██║   ██║   ██║   
+    ██║  ██║╚██████╗          ██████╔╝╚██████╔╝   ██║   
+    ╚═╝  ╚═╝ ╚═════╝          ╚═════╝  ╚═════╝    ╚═╝   
+    ┌──────────────────────────────────────────────────┐
+    │                  By Heart candy                  │
+    └──────────────────────────────────────────────────┘
+    `));
+}
+
+displaySystemInfo();
