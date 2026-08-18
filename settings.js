1fb66a74dbb5b18100b3c8edfecf70811932960c
Hc <rna376388@gmail.com>
2026-08-19 01:43:26 +0800

    update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  botName = ['Hc-bot-master_v2'],
-  ownerNumber = ['2685823709413'],
+  botName: ['Hc-bot-master_v2'],
+  ownerNumber: ['2685823709413'],
   pairing_code: true,
-  prefix = ['+',',','.','!',],
-  autoRead : false,
+  prefix: ['+', ',', '.', '!'],
+  autoRead: false,
   selfMode: false
 };
 
